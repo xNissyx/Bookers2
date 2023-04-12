893d56a85f73ae96bf6e3ae828f1e35a04d36954
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-04-12 06:15:21 +0000

    [finish] CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @book.save
     redirect_to book_path(@book.id)
   end
-  
+
   def index
     @user = current_user
     @users = User.all
@@ -17,14 +17,25 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
   def destroy
   end
-  
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
   
-  has_one_attached :image
+  has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
   def get_image
-    if image.attached?
-      image
+    if profile_image.attached?
+      profile_image
     else
       'default-image.jpg'
     end
